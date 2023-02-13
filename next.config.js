a2c9ab680565ad0b1b9edd7bc8e5976cc54d433a
Vivek Gupta <Vivekg@regenesys.net>
2023-02-13 19:58:17 +0530

    student-ui-setup

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/student",
+  basePath: '/student',
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    auth_Url:process.env.NEXT_PUBLIC_Auth_URL,
-    base_Url: process.env.NEXT_PUBLIC_BASE_URL,
+  trailingSlash: true,
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/student',
+        basePath: false,
+        permanent: false
+      }
+    ]
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  },
+  env: {
+    auth_Url: process.env.NEXT_PUBLIC_Auth_URL,
+    base_Url: process.env.NEXT_PUBLIC_BASE_URL
+  }
 }
 
 module.exports = nextConfig
