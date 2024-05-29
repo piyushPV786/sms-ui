2785ddfe3f83f13bde2ba4ab6966083139dd4e16
Apple <apple@ip-192-168-1-104.af-south-1.compute.internal>
2024-05-29 13:10:52 +0530

    fix

--- src/components/dialog/loginCridentialDialog.tsx
+++ src/components/dialog/loginCridentialDialog.tsx
@@ -10,6 +10,7 @@ import { GreenFormHeading } from '../common/style'
 
 const LoginCredentialDialog = ({ openCredentialDialog, setOpenCredentialDialog, applicationDetail }: any) => {
   // const url: any = process.env.NEXT_PUBLIC_CREDENTIAL_URL + RoutePaths.StudentDashboard
+console.log(applicationDetail, "-------applicationDetail---------------");
 
   return (
     <div>
@@ -64,11 +65,11 @@ const LoginCredentialDialog = ({ openCredentialDialog, setOpenCredentialDialog, 
             <Grid container md={12} sm={12} className='mt-3' rowSpacing={2} columnSpacing={10}>
               <Grid item md={6} sm={6} className='text-center'>
                 <p>Username</p>
-                <h5>{applicationDetail?.username}</h5>
+                <h5>{applicationDetail?.lead?.student?.userName}</h5>
               </Grid>
               <Grid item md={6} sm={6} className='text-center'>
                 <p>Password</p>
-                <h5>{applicationDetail?.password}</h5>
+                <h5>{applicationDetail?.lead?.student?.password}</h5>
               </Grid>
             </Grid>
           </Grid>
